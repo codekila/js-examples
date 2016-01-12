2970f6610c8905625a2d3c5a8bb84f117b6b0487
codekila <nctu_fsho@yahoo.com.tw>
2016-01-11 20:26:47 -0800

    minor fix

--- net/http1.ts
+++ net/http1.ts
@@ -1,9 +1,8 @@
 
-http = require("http");
+var http = require("http");
 
 var s = http.createServer(requestListner);
 s.listen(8080);
-
 
 function requestListner(req, res) {
     console.log("incoming request " + req.method + " from " + req.url);
