699091534358001db66d1d026baafe3e114cb8cf
codekila <nctu_fsho@yahoo.com.tw>
2016-01-12 23:52:55 -0800

    revised the recursive file search

--- path/path1.ts
+++ path/path1.ts
@@ -12,12 +12,20 @@ console.log(__dirname);
 console.log(util.inspect(process.argv));
 
 // return all files under a dir
-var pathExp = path.resolve("../../") + "/**";
-glob(pathExp, {nodir: true}, function(err, files) {
-    if (err) {
-        console.log(err.message);
-    }
-    else {
-        console.log(files);
-    }
-});
+(function(topDir:string, fileType:string) {
+    var pathExp:string = path.resolve(topDir);
+    console.log("Listing all files under " + pathExp + " matching " + fileType);
+    glob(pathExp + "/**/" + fileType, {nodir: true}, function (err, files) {
+        if (err) {
+            console.log(err.message);
+        }
+        else {
+            for (var i in files) {
+                var idx:number = files[i].lastIndexOf("/");
+                var fileName:string = files[i].slice(idx + 1);
+                var pathName:string = files[i].slice(0, idx);
+                console.log(pathName + " : " + fileName);
+            }
+        }
+    });
+})("../../", "*.ts");
