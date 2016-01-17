0785ead7c96b96941eb4a74684714dd457f97751
codekila <nctu_fsho@yahoo.com.tw>
2016-01-16 18:44:50 -0800

    added url.parse()

--- net/http1.ts
+++ net/http1.ts
@@ -1,12 +1,27 @@
 
 var http = require("http");
+var url = require("url");
+var process = require("process");
+//var util = require("util");
 
 var s = http.createServer(requestListner);
 s.listen(8080);
 
 function requestListner(req, res) {
-    console.log("incoming request " + req.method + " from " + req.url);
+    req.parsedURL = url.parse(req.url, true);
 
-    res.writeHead(200, { "Content-Type" : "application/json" });
-    res.end(JSON.stringify( { error: null, bbb: 123 }) + "\n");
+    //console.log(util.inspect(req.parsedURL));
+
+    var coreURL = req.parsedURL.pathname;
+    console.log("incoming request " + req.method + " from " + coreURL);
+
+    res.writeHead(200, {"Content-Type": "application/json"});
+    if (coreURL == '/' && req.parsedURL.query.hasOwnProperty('exit')) {
+        res.end(JSON.stringify({error: null, exit: true}) + "\n");
+        console.log("Exiting the http server...");
+        process.exit(0);
+    }
+    else {
+        res.end(JSON.stringify({error: null, continue: true}) + "\n");
+    }
 }
